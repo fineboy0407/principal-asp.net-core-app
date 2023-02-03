83ba8152837eaef51258289318b7479d3ec3a0d7
fineboy0407 <122990468+fineboy0407@users.noreply.github.com>
2023-02-04 07:06:13 +0900

    set up the solution

---
+++ principal-asp.net-core-app/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace principal_asp.net_core_app.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
